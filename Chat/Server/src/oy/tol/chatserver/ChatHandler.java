1be7f5b8a5341ef8a3cfbf1c7ca182bf02bbda6e
Antti Juustila <antti.juustila@gmail.com>
2020-12-23 13:15:36 +0200

    Finished working for today.

--- Chat/Server/src/oy/tol/chatserver/ChatHandler.java
+++ Chat/Server/src/oy/tol/chatserver/ChatHandler.java
@@ -1,16 +1,35 @@
 package oy.tol.chatserver;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class ChatHandler implements HttpHandler {
 
+	private ArrayList messages;
+	
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+		
+		if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+			InputStream in = exchange.getRequestBody();
+			Headers headers = exchange.getRequestHeaders();
+			// Check the content-length from header
+			// allocate room for content bytes
+			// Create a string from bytes
+			// Add to messages.
+		} else if (exchange.getRequestMethod().equalsIgnoreCase("GET")) {
+			// Create a string from all messages (or latest 100)
+			// Calculate length from the string
+			// write to os			
+		}
+		// TODO: Handle errors in post/get, and if request is not supported.
+		// TODO: Write simple client which can get and post.
 		String response = "Hello Chat World!";
 		exchange.sendResponseHeaders(200, response.length());
 		OutputStream os = exchange.getResponseBody();
