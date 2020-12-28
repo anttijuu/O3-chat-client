7889eef0885c36fb6bfb2d41e46bbd2e46aaf7b2
Antti Juustila <antti.juustila@gmail.com>
2020-12-28 12:33:48 +0200

    Adding GET and POST support to chat context

--- Chat/Server/src/oy/tol/chatserver/ChatHandler.java
+++ Chat/Server/src/oy/tol/chatserver/ChatHandler.java
@@ -1,9 +1,13 @@
 package oy.tol.chatserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
@@ -11,30 +15,49 @@ import com.sun.net.httpserver.HttpHandler;
 
 public class ChatHandler implements HttpHandler {
 
-	private ArrayList messages;
+	private ArrayList<String> messages = new ArrayList<String>();
 	
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+		int code = 200;
+		String messageBody = "";
 		
 		if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
-			InputStream in = exchange.getRequestBody();
 			Headers headers = exchange.getRequestHeaders();
-			// Check the content-length from header
-			// allocate room for content bytes
-			// Create a string from bytes
-			// Add to messages.
+			if (headers.containsKey("Content-Length")) {
+				int contentLength = Integer.parseInt(headers.get("Content-Length").get(0));
+				InputStream stream = exchange.getRequestBody();
+				String text = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))
+					        .lines()
+					        .collect(Collectors.joining("\n"));
+				stream.close();
+				if (text.length() > 0) {
+					messages.add(text);
+				}
+				exchange.sendResponseHeaders(code, -1);
+			} else {
+				code = 411;
+				messageBody = "Content-Length required.";
+			}
 		} else if (exchange.getRequestMethod().equalsIgnoreCase("GET")) {
-			// Create a string from all messages (or latest 100)
-			// Calculate length from the string
-			// write to os			
+			for (String message : messages) {
+				messageBody += message + "\n";
+			}
+			byte [] bytes = messageBody.getBytes("UTF-8");
+			exchange.sendResponseHeaders(code, bytes.length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(bytes);
+			os.close();
+		} else {
+			code = 400;
+			messageBody = "Not supported.";
 		}
-		// TODO: Handle errors in post/get, and if request is not supported.
-		// TODO: Write simple client which can get and post.
-		String response = "Hello Chat World!";
-		exchange.sendResponseHeaders(200, response.length());
-		OutputStream os = exchange.getResponseBody();
-		os.write(response.getBytes());
-		os.close();
+		if (code != 200) {
+			exchange.sendResponseHeaders(code, messageBody.length());
+			OutputStream os = exchange.getResponseBody();
+			os.write(messageBody.getBytes());
+			os.close();
+		}
 	}
 
 }
