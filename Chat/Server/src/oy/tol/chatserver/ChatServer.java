bfa60befbd465a06fe8a9b3ec2dd2fe1f7135753
Antti Juustila <antti.juustila@gmail.com>
2020-12-29 14:53:24 +0200

    Server is now https

--- Chat/Server/src/oy/tol/chatserver/ChatServer.java
+++ Chat/Server/src/oy/tol/chatserver/ChatServer.java
@@ -1,15 +1,67 @@
 package oy.tol.chatserver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.TrustManagerFactory;
+
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
 
 public class ChatServer {
 
 	public static void main(String[] args) throws Exception {
-		HttpServer server = HttpServer.create(new InetSocketAddress(8001), 0);
-		server.createContext("/chat", new ChatHandler());
-		server.setExecutor(null);
-		server.start();
+		try {
+			HttpsServer server = HttpsServer.create(new InetSocketAddress(8001), 0);
+			SSLContext sslContext = chatServerSSLContext();
+			server.setHttpsConfigurator (new HttpsConfigurator(sslContext) {
+		        public void configure (HttpsParameters params) {
+		        // get the remote address if needed
+		        InetSocketAddress remote = params.getClientAddress();
+		        SSLContext c = getSSLContext();
+		        // get the default parameters
+		        SSLParameters sslparams = c.getDefaultSSLParameters();
+//		        if (remote.equals (...) ) {
+//		            // modify the default set for client x
+//		        }
+		        params.setSSLParameters(sslparams);
+		        // statement above could throw IAE if any params invalid.
+		        // eg. if app has a UI and parameters supplied by a user.
+		        }
+		    });
+			server.createContext("/chat", new ChatHandler());
+			server.setExecutor(null);
+			server.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	private static SSLContext chatServerSSLContext() throws Exception {
+		char[] passphrase = "s3rver-secr3t-d0no7-xp0s3".toCharArray();
+		KeyStore ks = KeyStore.getInstance("JKS");
+		ks.load(new FileInputStream("keystore.jks"), passphrase);
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+		kmf.init(ks, passphrase);
+
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+		tmf.init(ks);
+
+		SSLContext ssl = SSLContext.getInstance("TLS");
+		ssl.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+		return ssl;
+	}
 }
